d2d222663e199bfc176581903d1d224654d53545
rkdms0527 <71059810+rkdms0527@users.noreply.github.com>
2020-11-26 15:20:34 +0900

    1

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+struct student {
+	int ID;
+	char name[100];
+	double grade;
+};
+
+int main (void) {
+	struct student s1 = {123456, "gaeun Park", 4.3};
+	
+	s1.ID = 456123;
+	s1.grade = 4.5;
+	
+	printf("ID : %d\n", s1.ID);
+	printf("name : %s\n", s1.name);
+	printf("grade : %f\n", s1.grade);
+}
+
